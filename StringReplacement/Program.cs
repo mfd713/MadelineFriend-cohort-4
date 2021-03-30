5f59fbe5d0bef655188772b494f7013d14ff0de5
mfd713 <friendmadeline@gmail.com>
2021-03-30 14:03:31 -0500

    added string replacement exercise

---
+++ StringReplacement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringReplacement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalString, searchString, replaceString, result;
+
+            //get OG string input from user
+            Console.Write("Enter a string: ");
+            originalString = Console.ReadLine();
+            //get search string from user
+            Console.Write("Enter a search string: ");
+            searchString = Console.ReadLine();
+            //get replacement string from user
+            Console.Write("Enter a replacement string: ");
+            replaceString = Console.ReadLine();
+
+            //use replace() to replace
+            result = originalString.Replace(searchString, replaceString);
+            //print final result
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
