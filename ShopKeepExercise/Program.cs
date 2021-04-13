efe933a2c579145808b37df53bd4c092c0ccd0c7
mfd713 <friendmadeline@gmail.com>
2021-04-13 16:09:47 -0500

    set up shopkeeper exercise

---
+++ ShopKeepExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShopKeepExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
