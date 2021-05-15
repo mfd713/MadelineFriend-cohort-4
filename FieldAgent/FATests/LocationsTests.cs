3e160e1ecbca1bf1d456e8f133c49f50988c5498
mfd713 <friendmadeline@gmail.com>
2021-05-15 15:44:49 -0500

    Finished Location repo and tests

---
+++ FieldAgent/FATests/LocationsTests.cs
@@ -0,0 +1,177 @@
+﻿using FieldAgent.DAL;
+using NUnit.Framework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using FieldAgent.Entities;
+using FieldAgent;
+using System.Collections.Generic;
+using System;
+
+namespace FAtests
+{
+    public class LocationsTests
+    {
+        private LocationEFRepo locationRepo;
+        private FieldAgentsDbContext context;
+        private Location testLocation;
+
+        private static FieldAgentsDbContext GetDbContext()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddInMemoryCollection();
+
+            var options = new DbContextOptionsBuilder()
+                .UseInMemoryDatabase("testDatabase")
+                .Options;
+
+            return new FieldAgentsDbContext(options);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            context = GetDbContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            locationRepo = new LocationEFRepo(context);
+            testLocation = new Location
+            {
+                LocationName = "Starbucks",
+                Street1 = "2700 N Clark St",
+                City = "Chicago",
+                PostalCode = "60614",
+                CountryCode = "01"
+            };
+        }
+
+        [Test]
+        public void InsertShouldAddLocationtoDB()
+        {
+            Response<Location> response = locationRepo.Insert(testLocation);
+
+            Assert.IsTrue(response.Success);
+            Assert.NotZero(response.Data.LocationId);
+        }
+
+        [Test]
+        public void GetShouldReturnLocation()
+        {
+            locationRepo.Insert(testLocation);
+
+            Response<Location> response = locationRepo.Get(1);
+
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual("Starbucks", response.Data.LocationName);
+        }
+
+        [Test]
+        public void GetWithInvalidIDShouldReturnFail()
+        {
+            locationRepo.Insert(testLocation);
+
+            Response<Location> response = locationRepo.Get(2);
+
+            Assert.IsFalse(response.Success);
+            Assert.AreEqual("Location with ID 2 not found", response.Message);
+            Assert.IsNull(response.Data);
+        }
+
+        [Test]
+        public void GetAgencyShouldReturnLocationList()
+        {
+            Agency agency = new Agency
+            {
+                AgencyId = 1,
+                ShortName = "Test",
+                LongName = "Testley",
+                Locations = new List<Location>()
+            };
+
+            testLocation.AgencyId = 1;
+            agency.Locations.Add(testLocation);
+
+            locationRepo.Insert(testLocation);
+            Response<List<Location>> response = locationRepo.GetByAgency(1);
+
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual(1, response.Data.Count);
+            Assert.AreEqual("Starbucks", response.Data[0].LocationName);
+        }
+
+        [Test]
+        public void GetWithInvalidAgencyShouldReturnFail()
+        {
+            Agency agency = new Agency
+            {
+                AgencyId = 1,
+                ShortName = "Test",
+                LongName = "Testley",
+                Locations = new List<Location>()
+            };
+
+            testLocation.AgencyId = 1;
+            agency.Locations.Add(testLocation);
+
+            locationRepo.Insert(testLocation);
+            Response<List<Location>> response = locationRepo.GetByAgency(2);
+
+            Assert.IsFalse(response.Success);
+            Assert.Zero(response.Data.Count);
+            Assert.AreEqual("No Locations with Agency ID 2 found", response.Message);
+        }
+
+        [Test]
+        public void UpdateShouldUpdateLocation()
+        {
+            Location toUpdate = locationRepo.Insert(testLocation).Data;
+
+            toUpdate.City = "Temecula";
+            Response response = locationRepo.Update(toUpdate);
+
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual("Temecula", locationRepo.Get(1).Data.City);
+        }
+
+        [Test]
+        public void UpdateShouldReturnFailForInvalidLocation()
+        {
+            locationRepo.Insert(testLocation);
+            Location toUpdate = new Location
+            {
+                City = "Temecula",
+                LocationId = 2
+            };
+
+            Response response = locationRepo.Update(toUpdate);
+
+            Assert.IsFalse(response.Success);
+            Assert.AreEqual("Location with ID 2 not found", response.Message);
+            Assert.AreEqual("Chicago", locationRepo.Get(1).Data.City);
+        }
+
+        [Test]
+        public void DeleteShouldDeleteLocation()
+        {
+            locationRepo.Insert(testLocation);
+
+            Response response = locationRepo.Delete(1);
+
+            Assert.IsTrue(response.Success);
+            Assert.IsNull(locationRepo.Get(1).Data);
+        }
+
+        [Test]
+        public void DeleteShouldReturnFailForInvalidLocation()
+        {
+            locationRepo.Insert(testLocation);
+
+            Response response = locationRepo.Delete(2);
+
+            Assert.IsFalse(response.Success);
+            Assert.AreEqual("Location with ID 2 not found", response.Message);
+            Assert.AreEqual("Chicago", locationRepo.Get(1).Data.City);
+        }
+    }
+}
---
+++ FieldAgent/FieldAgent.DAL/LocationEFRepo.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FieldAgent.Entities;
+using FieldAgent.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FieldAgent.DAL
+{
+    public class LocationEFRepo : ILocationRepository
+    {
+        private FieldAgentsDbContext context;
+
+        public LocationEFRepo(FieldAgentsDbContext context)
+        {
+            this.context = context;
+        }
+
+        public Response Delete(int locationId)
+        {
+            Response response = new Response();
+            Location toDelete = context.Location.Find(locationId);
+            try
+            {
+                context.Location.Remove(toDelete);
+                response.Success = context.SaveChanges() == 1;
+            }
+            catch
+            {
+                response.Success = false;
+                response.Message = $"Location with ID {locationId} not found";
+                return response;
+            }
+            if (!response.Success)
+            {
+                response.Message = $"Location with ID {locationId} not found";
+            }
+
+            return response;
+        }
+
+        public Response<Location> Get(int locationId)
+        {
+            Response<Location> response = new Response<Location>();
+
+            response.Data = context.Location.Find(locationId);
+
+            response.Success = response.Data != null;
+            if (!response.Success)
+            {
+                response.Message = $"Location with ID {locationId} not found";
+            }
+            return response;
+        }
+
+        public Response<List<Location>> GetByAgency(int agencyId)
+        {
+            Response<List<Location>> response = new Response<List<Location>>();
+
+            response.Data = context.Location
+                .Where(l => l.AgencyId == agencyId)
+                .ToList();
+            response.Success = response.Data.Count > 0;
+            if (!response.Success)
+            {
+                response.Message = $"No Locations with Agency ID {agencyId} found";
+            }
+            return response;
+        }
+
+        public Response<Location> Insert(Location location)
+        {
+            Response<Location> response = new Response<Location>();
+
+            try
+            {
+                context.Location.Add(location);
+                response.Success = context.SaveChanges() == 1;
+            }
+            
+            catch
+            {
+                response.Message = "Could not add Location";
+                response.Success = false;
+                return response;
+            }
+            response.Data = location;
+            return response;
+        }
+
+        public Response Update(Location location)
+        {
+            Response response = new Response();
+            try
+            {
+                context.Location.Update(location);
+                response.Success = context.SaveChanges() == 1;
+            }
+            catch
+            {
+                response.Success = false;
+                response.Message = $"Location with ID {location.LocationId} not found";
+                return response;
+            }
+            if (!response.Success)
+            {
+                response.Message = $"Location with ID {location.LocationId} not found";
+            }
+
+            return response;
+        }
+    }
+}
