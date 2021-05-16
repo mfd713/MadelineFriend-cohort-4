8958b50efb63c145e07c2cf281381b5bee17c1cd
mfd713 <friendmadeline@gmail.com>
2021-05-16 16:13:06 -0500

    Reports repo and tests working

---
+++ FieldAgent/FATests/ReportsTests.cs
@@ -0,0 +1,100 @@
+﻿using FieldAgent.DAL;
+using NUnit.Framework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using FieldAgent.Entities;
+using FieldAgent;
+using System.Collections.Generic;
+using System;
+
+namespace FAtests
+{
+    public class ReportsTests
+    {
+        private string connectionString;
+        private ReportsADORepository reportsRepo;
+
+        private static string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddUserSecrets<FieldAgentsDbContext>();
+
+            var config = builder.Build();
+
+            string connectionString = config["ConnectionStrings:FieldAgent"];
+            return connectionString;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            connectionString = GetConnectionString();
+            reportsRepo = new ReportsADORepository(connectionString);
+        }
+
+        [Test]
+        public void GetTopAgentsShouldReturnAgentList()
+        {
+            var response = reportsRepo.GetTopAgents();
+
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual(3, response.Data.Count);
+            Assert.AreEqual(4, response.Data[0].CompletedMissionCount);
+        }
+
+        [Test]
+        public void GetAgentsFromBadConnectionShouldReturnFail()
+        {
+            var emptyRepo = new ReportsADORepository("");
+            var response = emptyRepo.GetTopAgents();
+
+            Assert.IsFalse(response.Success);
+            Assert.Zero(response.Data.Count);
+            Assert.AreEqual("Database empty", response.Message);
+        }
+
+        [Test]
+        public void GetPensionListShouldReturnAgents()
+        {
+            var response = reportsRepo.GetPensionList(2);
+
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual(1, response.Data.Count);
+            Assert.IsTrue(response.Data[0].NameLastFirst.Contains("Lanna"));
+        }
+
+        [Test]
+        public void GetPensionFromNoPensionersShouldReturnFail()
+        {
+            var emptyRepo = new ReportsADORepository("");
+            var response = emptyRepo.GetPensionList(1);
+
+            Assert.IsFalse(response.Success);
+            Assert.Zero(response.Data.Count);
+            Assert.AreEqual("No results for that query", response.Message);
+        }
+
+        [Test]
+        public void AuditClearanceShouldReturnListOfObjects()
+        {
+            var response = reportsRepo.AuditClearance(4, 1);
+
+            Assert.IsTrue(response.Success);
+            Assert.AreEqual(1, response.Data.Count);
+            Assert.IsTrue(response.Data[0].NameLastFirst.Contains("Bond"));
+            Assert.IsNull(response.Data[0].DeactivationDate);
+        }
+
+        [Test]
+        public void AuditClearanceEmptySearchShouldReturnFail()
+        {
+            var emptyRepo = new ReportsADORepository("");
+            var response = emptyRepo.AuditClearance(1, 1);
+
+            Assert.IsFalse(response.Success);
+            Assert.Zero(response.Data.Count);
+            Assert.AreEqual("No results for that query", response.Message);
+        }
+    }
+}
--- FieldAgent/FieldAgent.DAL/ReportsADORepository.cs
+++ FieldAgent/FieldAgent.DAL/ReportsADORepository.cs
@@ -3,24 +3,146 @@ using System.Linq;
 using FieldAgent.Entities;
 using FieldAgent.Interfaces;
 using FieldAgent.DTOs;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System;
 
 namespace FieldAgent.DAL
 {
     public class ReportsADORepository : IReportsRepository
     {
+        private string connectionString;
+
+        public ReportsADORepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
         public Response<List<ClearanceAuditListItem>> AuditClearance(int securityClearanceId, int agencyId)
         {
-            throw new System.NotImplementedException();
+            Response<List<ClearanceAuditListItem>> response = new Response<List<ClearanceAuditListItem>>();
+            response.Data = new List<ClearanceAuditListItem>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand("ClearanceAudit", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@agencyId", agencyId);
+                command.Parameters.AddWithValue("@securityClearanceId", securityClearanceId);
+                try
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = new ClearanceAuditListItem();
+
+                            row.NameLastFirst = $"{reader["LastName"]},{reader["FirstName"]}";
+                            row.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
+                            row.BadgeId = new Guid(reader["BadgeId"].ToString());
+                            row.ActivationDate = DateTime.Parse(reader["ActivationDate"].ToString());
+
+                            if (reader["DeactivationDate"] != DBNull.Value)
+                            {
+                                row.DeactivationDate = DateTime.Parse(reader["DeactivationDate"].ToString());
+                            }
+
+
+                            response.Data.Add(row);
+                        }
+
+                    }
+                }
+                catch
+                {
+                    response.Message = $"Could not connect to database";
+                    response.Success = false;
+                }
+
+                response.Success = response.Data.Count > 0;
+                response.Message = response.Success ? "" : "No results for that query";
+                return response;
+            }
         }
 
         public Response<List<PensionListItem>> GetPensionList(int agencyId)
         {
-            throw new System.NotImplementedException();
+            Response<List<PensionListItem>> response = new Response<List<PensionListItem>>();
+            response.Data = new List<PensionListItem>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand("PensionList", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@agencyId", agencyId);
+                try
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = new PensionListItem();
+
+                            row.NameLastFirst = $"{reader["LastName"]},{reader["FirstName"]}";
+                            row.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
+                            row.BadgeId = new Guid(reader["BadgeId"].ToString());
+                            row.DeactivationDate = DateTime.Parse(reader["DeactivationDate"].ToString());
+                            row.AgencyName = reader["ShortName"].ToString();
+
+                            response.Data.Add(row);
+                        }
+
+                    }
+                }
+                catch
+                {
+                    response.Message = $"Could not connect to database";
+                    response.Success = false;
+                }
+
+                response.Success = response.Data.Count > 0;
+                response.Message = response.Success ? "" : "No results for that query";
+                return response;
+            }
         }
 
         public Response<List<TopAgentListItem>> GetTopAgents()
         {
-            throw new System.NotImplementedException();
+            Response<List<TopAgentListItem>> response = new Response<List<TopAgentListItem>>();
+            response.Data = new List<TopAgentListItem>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand("TopAgents", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                try
+                {
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = new TopAgentListItem();
+
+                            row.NameLastFirst = $"{reader["LastName"]},{reader["FirstName"]}";
+                            row.DateOfBirth = DateTime.Parse(reader["DateOfBirth"].ToString());
+                            row.CompletedMissionCount = (int)reader["Completed"];
+
+                            response.Data.Add(row);
+                        }
+
+                    }
+                }
+                catch
+                {
+                    response.Message = $"Could not connect to database";
+                    response.Success = false;
+                }
+
+                response.Success = response.Data.Count > 0;
+                response.Message = response.Success ? "" : "Database empty";
+                return response;
+            }
         }
     }
 }
